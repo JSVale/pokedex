f5ca13841bdaf61678a479a57ae804a3bd3ae862
Lucas Mallmann <lucasmallmann76@gmail.com>
2021-01-04 20:50:25 -0300

    feat: initial structure

---
+++ src/pages/_app.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { AppProps } from 'next/app'
+
+import '../styles/reset.css'
+import '../styles/global.css'
+
+const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
+  return <Component {...pageProps} />
+}
+
+export default MyApp
---
+++ src/pages/_document.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+export default class MyDocument extends Document {
+  render(): JSX.Element {
+    return (
+      <Html lang="pt">
+        <Head>
+          <meta charSet="utf-8" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+
+          <link
+            href="https://fonts.googleapis.com/css2?family=Inter:wght@300;500;700&display=swap"
+            rel="stylesheet"
+          />
+
+          <link rel="icon" href="https://rocketseat.com.br/favicon.ico" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
